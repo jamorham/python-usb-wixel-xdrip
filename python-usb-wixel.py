0a8f8e8ca55a7f9d41de68b79cc5e8b9821372b9
Jamorham <jamorham@users.noreply.github.com>
2016-02-22 21:06:47 +0000

    Added initial support for:
    Raspberry Pi Native serial port (instead of via USB)
    Uploading data to Parakeet App Engine instance
    Windows Python compatibility
    Loading settings from python-usb-wixel.cfg file - example included

--- python-usb-wixel.py
+++ python-usb-wixel.py
@@ -29,20 +29,77 @@ import socket
 import sys
 import time
 import os
-import grp
+from ConfigParser import SafeConfigParser
+import platform
 from urlparse import urlparse
 import threading
 import signal
 import serial
+import pycurl
+from StringIO import StringIO
+import re
+
+if platform.system() != "Windows":
+	import grp
+
+	DEFAULT_LOG_FILE = "/tmp/python-usb-wixel.log"
+else:
+	DEFAULT_LOG_FILE = "python-usb-wixel.log"
 
 HOST = ''  # All interfaces
 PORT = 50005  # xdrip standard port
 
-# Set a list of remote hosts in the format "192.168.1.50:50005", ... to consolidate
+# Set a list of remote hosts in the format 192.168.1.50:50005, ... to consolidate
 # multiple remote usb wixels or parakeet webservices to a single instance
 # the script will poll the others for the latest data every 10 seconds.
 
-remoteHosts = []
+remoteHosts = ""
+
+# Set the parakeet url to your receiver.cgi url, eg https://your-parakeet-receiver.appspot.com/receiver.cgi
+# and then all received data will be relayed to a parakeet app engine instance which is good for bypassing
+# network address translation etc. App engine software is at: https://github.com/jamorham/Parakeet-App-Engine
+
+parakeet_url = ""
+parakeet_passcode = "12345"
+
+# If you wired your wixel directly to the serial pins of the raspberry Pi set this to True
+# for usb connected wixels leave it set as False
+
+use_raspberry_pi_internal_serial_port = False
+
+# Or you can store the configuration in a file which overrides whatever is set in this script
+
+config = SafeConfigParser({'HOST': HOST,
+						   'PORT': PORT,
+						   'remoteHosts': remoteHosts,
+						   'parakeet_url': parakeet_url,
+						   'parakeet_passcode': parakeet_passcode,
+						   'use_raspberry_pi_internal_serial_port': False,
+						   'DEFAULT_LOG_FILE': DEFAULT_LOG_FILE})
+
+# script should be python-usb-wixel.py and then config file will be python-usb-wixel.cfg
+config_path = re.sub(r".py$", ".cfg", os.path.realpath(__file__))
+
+if (os.path.isfile(config_path)):
+	config.read(config_path)
+	print "Loading configuration from: " + config_path
+	HOST = config.get('main', 'HOST').strip()
+	PORT = config.getint('main', 'PORT')
+	remoteHosts = config.get('main', 'remoteHosts').strip()
+	parakeet_url = config.get('main', 'parakeet_url').strip()
+	try:
+		use_raspberry_pi_serial_port = config.getboolean('main', 'use_raspberry_pi_internal_serial_port')
+	except:
+		use_raspberry_pi_internal_serial_port = False
+	DEFAULT_LOG_FILE = config.get('main', 'DEFAULT_LOG_FILE').strip()
+else:
+	print "No custom config file: " + config_path
+
+# remoteHosts is now specified as , separated string then converted to old style list
+if (len(remoteHosts) > 0):
+	remoteHosts = remoteHosts.split(',')
+else:
+	remoteHosts = []
 
 # output template
 
@@ -71,17 +128,19 @@ def serialThread(dummy):
 				if os.path.exists("/dev/ttyACM1"):
 					ser = serial.Serial('/dev/ttyACM1', 9600)
 				else:
-					logger.error("Could not find any /dev/ttyACMx device")
-					time.sleep(30)
+					if use_raspberry_pi_internal_serial_port and os.path.exists("/dev/ttyAMA0"):
+						ser = serial.Serial('/dev/ttyAMA0', 9600)
+					else:
+						logger.error("Could not find any serial device")
+						time.sleep(30)
 
 			try:
 				serial_line = ser.readline()
 
-
 				# debug print what we received
 				print serial_line
+				serial_line = re.sub("[^0-9 \n-]", "", serial_line)
 				logger.info("Serial line: " + serial_line.strip())
-
 
 				# simple space delimited data records
 				datax = serial_line.split(" ")
@@ -101,8 +160,11 @@ def serialThread(dummy):
 				mydata['BatteryLife'] = datax[3]
 				mydata['ReceivedSignalStrength'] = datax[4]
 				mydata['TransmissionId'] = datax[5]
+
+				parakeet_upload()
+
 			except Exception, e:
-				print "Exception: ",e
+				print "Exception: ", e
 
 		except serial.serialutil.SerialException, e:
 			print "Serial exception ", e
@@ -111,9 +173,10 @@ def serialThread(dummy):
 		try:
 			ser.close()
 		except Exception, e:
-			print "Serial close exception",e
+			print "Serial close exception", e
 
 		time.sleep(6)
+
 
 # socket thread
 
@@ -140,9 +203,8 @@ def clientThread(connlocal):
 			connlocal.sendall(reply)
 		connlocal.close()
 
-	except Exception,e:
-		print "Exception in clientThread: ",e
-
+	except Exception, e:
+		print "Exception in clientThread: ", e
 
 
 def consolidationThread():
@@ -153,11 +215,11 @@ def consolidationThread():
 		for host in remoteHosts:
 			try:
 
-				host = "//"+host # must be cleaner way to do this
-				remote_address = (urlparse(host).hostname,urlparse(host).port)
-				#print "Trying: ",remote_address
-				sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-				sock.settimeout(5) 	# could be longer if not on local network
+				host = "//" + host  # must be cleaner way to do this
+				remote_address = (urlparse(host).hostname, urlparse(host).port)
+				# print "Trying: ",remote_address
+				sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				sock.settimeout(5)  # could be longer if not on local network
 
 				sock.connect(remote_address)
 
@@ -167,19 +229,54 @@ def consolidationThread():
 
 				decoded_data = json.loads(data)
 				if int(decoded_data['RelativeTime']) < ((int(time.time()) * 1000) - int(mydata['CaptureDateTime'])):
-
-					#print "Received NEWER: ",data
-					print "NEWEST FROM: ",remote_address
+					# print "Received NEWER: ",data
+					print "NEWEST FROM: ", remote_address
 					mydata = decoded_data
+					parakeet_upload()
 
 				sock.close()
 
 			except Exception, e:
-				print e,remote_address
-
-
+				print e, remote_address
 
 		time.sleep(10)
+
+
+def parakeet_upload():
+	global parakeet_url
+	global mydata
+	if parakeet_url != "":
+		tries = 0
+		max_tries = 2
+		success = False
+		while (success == False) and (tries < max_tries):
+			tries = tries + 1
+			buffer = StringIO()
+			c = pycurl.Curl()
+			c.setopt(pycurl.URL, parakeet_url
+					 + "?"
+					 + "&lv=" + mydata['RawValue']
+					 + "&lf=" + mydata['FilteredValue']
+					 + "&zi=" + mydata['TransmitterId']
+					 + "&db=" + mydata['BatteryLife']
+					 + "&pc=" + parakeet_passcode
+					 + "&ts=" + "1"
+					 )
+			c.setopt(c.TIMEOUT, 20)
+			c.setopt(c.USERAGENT, "usb-wixel")
+			c.setopt(c.WRITEDATA, buffer)
+			c.perform()
+			rcode = c.getinfo(c.RESPONSE_CODE)
+			rtime = c.getinfo(c.TOTAL_TIME)
+			c.close()
+			if (rcode == 200) and buffer.getvalue().startswith("!ACK"):
+				print "Successfully Uploaded parakeet data in " + str(rtime) + "s"
+				success = True
+			else:
+				logger.error(
+					"Error uploading Parakeet data: Response code: " + str(rcode) + " time: " + str(rtime) + "s")
+				logger.error("received reply: " + buffer.getvalue())
+				time.sleep(10)
 
 
 # threads end
@@ -187,26 +284,27 @@ def consolidationThread():
 # MAIN
 
 # some init
-
-if os.getuid() == 0:
-	print "Dropping root"
-	os.setgid(1000) # make sure this user is in the dialout group or setgid to dialout
-	try:
-		os.setgid(grp.getgrnam("dialout").gr_gid)
-	except:
-		print "Couldn't find the dialout group to use"
-
-	os.setuid(1000)
-	print "Dropped to user: ", os.getuid()
+if (platform.system() != "Windows"):
 	if os.getuid() == 0:
-		print "Cannot drop root - exit!"
-		sys.exit()
+		print "Dropping root"
+		os.setgid(1000)  # make sure this user is in the dialout group or setgid to dialout
+		try:
+			os.setgid(grp.getgrnam("dialout").gr_gid)
+		except:
+			print "Couldn't find the dialout group to use"
+
+		os.setuid(1000)
+		print "Dropped to user: ", os.getuid()
+		if os.getuid() == 0:
+			print "Cannot drop root - exit!"
+			sys.exit()
 
 logger = logging.getLogger('python-usb-wixel')
-hdlr = logging.FileHandler('/tmp/python-usb-wixel.log')
+hdlr = logging.FileHandler(DEFAULT_LOG_FILE)
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 hdlr.setFormatter(formatter)
 logger.addHandler(hdlr)
+logger.addHandler(logging.StreamHandler())
 
 # choose your logging level as required
 
@@ -214,7 +312,6 @@ logger.setLevel(logging.INFO)
 # logger.setLevel(logging.WARNING)
 
 logger.info("Startup")
-
 
 # Create socket
 
@@ -233,11 +330,10 @@ except socket.error as msg:
 
 s.listen(10)
 
-threading.Thread(target=serialThread,args=("",)).start()
+threading.Thread(target=serialThread, args=("",)).start()
 
-if len(remoteHosts)>0:
-	t=threading.Thread(target=consolidationThread,args=()).start()
-
+if len(remoteHosts) > 0:
+	t = threading.Thread(target=consolidationThread, args=()).start()
 
 # main busy loop
 # wait for connections and start a thread
@@ -248,9 +344,9 @@ try:
 		conn, addr = s.accept()
 		print 'Connected with ' + addr[0] + ':' + str(addr[1])
 
-		threading.Thread(target=clientThread,args=(conn,)).start()
+		threading.Thread(target=clientThread, args=(conn,)).start()
 	s.close()
 
 except KeyboardInterrupt:
 	print "Shutting down"
-	os.kill(os.getpid(),signal.SIGKILL)
+	os.kill(os.getpid(), signal.SIGKILL)
